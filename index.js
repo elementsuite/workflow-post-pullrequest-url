712005cf37edb41e2564655ce155b1e22a8a8d23
Samuel O'Donnell <samuel.odonnell@elementsuite.com>
2020-01-19 14:40:22 +0000

    initial poc

---
+++ index.js
@@ -0,0 +1,41 @@
+const core = require('@actions/core');
+const github = require('@actions/github');
+const request = require('request');
+
+if (!String.prototype.format) {
+  String.prototype.format = function() {
+    var args = arguments;
+    return this.replace(/{(\d+)}/g, function(match, number) {
+      return typeof args[number] != 'undefined'
+        ? args[number]
+        : match
+      ;
+    });
+  };
+}
+
+try {
+  const token = core.getInput('github-token');
+  const url = core.getInput('url');
+  const auth = core.getInput('auth');
+  const client = new github.GitHub(token);
+  const payload = github.context.payload;
+  const title = payload.pull_request.title;
+  const status = payload.action;
+
+  var attributes = title.split('/');
+  var id = attributes[2];
+  if (!id || isNaN(id)) {
+    return;
+  }
+  request({
+    url: url.format(id, status),
+    headers: {
+      "Authorization": auth
+    }
+  }, function(error, response, body) {
+    console.log('request', body);
+  });
+} catch (error) {
+  console.error(error.message);
+}
